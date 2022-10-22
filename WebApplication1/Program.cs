346594401e22c31bb20fa4a13a48d926b9d4926c
yesilyurtalper <yesilyurtalper@gmail.com>
2022-10-22 23:04:14 +0300

    program.cs commented

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var app = builder.Build();//commented
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
